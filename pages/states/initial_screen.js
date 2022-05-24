2b077565db2dcc500d1bae4a160fc07bd70986d5
Yue Hern <yuehernkang@hotmail.com>
2022-05-25 00:41:36 +0800

    updated

--- pages/states/initial_screen.js
+++ pages/states/initial_screen.js
@@ -2,10 +2,11 @@ import styled from 'styled-components';
 import { getRoom } from '../../services/firebase_service';
 import {LOBBY_SCREEN_STATE, LOGIN_SCREEN_STATE} from './states';
 import { useState } from 'react';
+import Modal from '@mui/material/Modal';
 
 export const InitialScreen = ({state, onStateChange}) => {
 
-    const [inputValue, setinputValue] = useState("");
+    const [inputValue, setinputValue] = useState();
 
     const [joinButtonState, setJoinButtonState] = useState(true);
 
@@ -23,21 +24,29 @@ export const InitialScreen = ({state, onStateChange}) => {
     }
 
     const onChange = (e) => {
-        setinputValue(e.target.value);
+        setinputValue(e.target.value.toUpperCase());
         if(e.target.value.length == 4) {
             setJoinButtonState(false);
+        } else{
+            setJoinButtonState(true);
         }
     }
 
-    const Title = styled.h1``;
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        getRoomData(event.target.value);
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>Enter Room ID
+                <input 
+                    type="text" 
+                    value={inputValue}
+                    onChange={onChange} 
+                />
+                <input type="submit" value="Submit" disabled={joinButtonState}/>
+            </label>
+        </form>
 
-    const InputBox = styled.input``;
-
-    const NiceButtons = styled.button``;
-    return (<Title>
-        Welcome To TikTok Game
-        <InputBox placeholder='Enter Game ID' value={inputValue.toUpperCase()} onChange={onChange}/>
-        <NiceButtons onClick={() => onStateChange(LOGIN_SCREEN_STATE)} >Create New Game</NiceButtons>
-        <NiceButtons onClick={() => getRoomData(inputValue)} disabled={joinButtonState}>Join Game</NiceButtons>
-      </Title>);
+    );
 }
